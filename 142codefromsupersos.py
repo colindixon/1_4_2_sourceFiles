21bf3f360e93d1b56a2b95ffe352e8e531e42baf
Colin Dixon <i'll haunt you donald trump>
2017-12-07 13:26:26 -0800

    added cat into code

--- 142codefromsupersos.py
+++ 142codefromsupersos.py
@@ -10,7 +10,7 @@ import numpy as np # “as” lets us use standard abbreviations
 # Get the directory of this python script
 directory = os.path.dirname(os.path.abspath(__file__)) 
 # Build an absolute filename from directory + filename
-filename = os.path.join(directory, 'woman.jpg')
+filename = os.path.join(directory, 'cat1-a.gif')
 # Read the image data into an array
 img = plt.imread(filename)
   
